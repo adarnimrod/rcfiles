aaeff19313be13f9623af77002aa859a3d3ce6df
Adar Nimrod <nimrod@shore.co.il>
2021-07-26 10:04:09 +0300

    For the gitlab-ci linter.

--- generated.mk
+++ generated.mk
@@ -67,6 +67,7 @@ generated: .bash_completion.d/poetry
 generated: .bashrc.private
 .bashrc.private: Documents/Database.kdbx
 	echo "export GITLAB_TOKEN='$$(ph show --field Password 'shore.co.il/GitLab token')'" > '$@'
+	echo 'export GITLAB_PRIVATE_TOKEN="$$GITLAB_TOKEN"' >> '$@'
 	echo "export GITLAB_REGISTRATION_TOKEN='$$(ph show --field Password 'shore.co.il/GitLab runner registration token')'" >> '$@'
 
 generated: .ssh/github_ed25519
